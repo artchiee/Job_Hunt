54cb58fb1ebd4769f665dbecb3d2722c5c9634bd
artchiee <53340008+artchiee@users.noreply.github.com>
2019-12-14 15:03:13 +0100

    commiting more code

--- .history/job_hunting_20191212194105.py
+++ .history/job_hunting_20191212194105.py
@@ -26,33 +26,27 @@ driver.get(start_url)
 job_field = driver.find_element_by_xpath('//*[@id="text-input-what"]')
 job_field.send_keys(job_title)
 
-# test here
+# test here 
 location_field = driver.find_element_by_xpath('//*[@id="text-input-where"]')
 location_field.send_keys(Keys.CONTROL + "a")
 location_field.send_keys(Keys.DELETE)
 
-global location_input
+def enter_location(location_field):
 
-location_input = ''
-
-
-def enter_location(location_field, give_location):
-    location_field = driver.find_element_by_xpath(
-        '//*[@id="text-input-where"]')
-
+    location_field = driver.find_element_by_xpath('//*[@id="text-input-where"]')
     try:
         if location_field == '':
-            location_input = str(
+            global location_input = str(
                 input('enter location or see all available jobs ' + '\n'))
             answer_me = str(
                 input('yes --> give location  ||| ' + 'no --> see all'))
-
-            print(location_input + answer_me)
+            
+            print(location_input  +  answer_me)
 
             #answer is yes
             if answer_me == 'yes':
-                give_location = str(input('Enter location'))
-                location_field.send_keys(give_location)
+                enter_location = str(input('Enter location'))
+                location_field.send_keys(enter_location)
                 location_field.send_keys(Keys.ENTER)
 
             # answer == no
@@ -62,33 +56,34 @@ def enter_location(location_field, give_location):
 
         # if location_field wes filled call the function bellow()
         else:
-            location_field.send_keys(Keys.CONTROL + "a")
-            location_field.send_keys(Keys.DELETE)
-            location_field.send_keys(Keys.ENTER)
+            clear_location_field()
             time.sleep(3)
 
-            print('give location now :' + "   " ) 
-            give_location
-            location_field.send_keys(give_location)
-            location_field.send_keys(Keys.ENTER)    
 
-    except: 
+
+
+    except:
         Exception()
-
 
 # clear location_field if it was filled (by defalute)
 
-# def clear_location_field():
-#     location_field = driver.find_element_by_xpath(
-#         '//*[@id="text-input-where"]')
+def clear_location_field():
+    location_field = driver.find_element_by_xpath(
+        '//*[@id="text-input-where"]')
 
-#     location_field.send_keys(Keys.CONTROL + "a")
-#     location_field.send_keys(Keys.DELETE)
-#     location_field.send_keys(Keys.ENTER)
+    location_field.send_keys(Keys.CONTROL + "a")
+    location_field.send_keys(Keys.DELETE)
+    location_field.send_keys(Keys.ENTER)
+
+    
+            # Now Give me option to enter location
+    print('Sort this jobs by location ' + '\n' + print(enter_location))
+    location_field.send_keys(enter_location)
+    location_field.send_keys(Keys.ENTER)
 
 
-# Move on to the secont page
+# Move on to the secont page 
 
 
 # call the two functions
-enter_location(location_field, give_location= 'casa')
+enter_location(location_field)
